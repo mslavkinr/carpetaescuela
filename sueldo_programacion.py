0f35a8a54395b041f17797dc68351947a50d5d34
mslavkinr <maxislavkin@gmail.com>
2025-11-13 12:36:53 -0300

    programacion python

---
+++ sueldo_programacion.py
@@ -0,0 +1,4 @@
+numh = float(input("Ingrese el numero de horas trabajadas"));
+costh = float(input("Ingrese el costo por hora"));
+sueldo = float(numh * costh);
+print (numh, "fueron las horas trabajadas,", costh, "fue el costo por hora,", sueldo, "fue el sueldo correspondiente");
